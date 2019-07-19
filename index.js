52b1193e0f429a6a85c903fa962148c480097726
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-19 11:28:29 -0400

    Part 3 completed

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ const image= document.createElement('img');
 
 // Set the source attribute of the image element to `https://i.imgur.com/5bMVQtj.png` and append it to the content div
 image.setAttribute('src',`https://i.imgur.com/5bMVQtj.png`);
+image.style.width = "600px";
 house.appendChild(image);
 // Center this image on the page using your favorite method. (I'm using flexbox, but you do you)
 house.style.display = `flex`;
@@ -43,7 +44,7 @@ house.style.alignItems = `center`;
 house.style.justifyContent = `center`;
 
   // Note: don't forget to give your content div a height like I did 😳
-  house.style.height = `25vh`;
+house.style.height = `40vh`;
 
 // Now's a good time to style the body background too, I guess
 body.style.backgroundColor = `aquamarine`;
@@ -56,13 +57,18 @@ body.style.backgroundColor = `aquamarine`;
   // Text: 'The man who runs may fight again.\n\t\t-- Menander'
   // Hint: This is going to require setting the content's position to relative first
   // Hint: Additionally, I styled the paragraph element's width, text alignment, top and left positions
-
+const txt = document.createElement('p');
+txt.style.position = "relative";
+// console.log(fortune['fortune']);
+house.appendChild(txt);
+txt.style.right = "350px";
+txt.style.bottom = "15px";
 // Make an Object (a.k.a a 'dictionary') called `fortune` and add one key-value pair:
-  // 'fortune' : 'The man who runs may fight again.\n\t\t-- Menander'
-
+// 'fortune' : 'The man who runs may fight again.\n\t\t-- Menander'
+const fortune = {'fortune' : 'The man who runs may fight again.\n\t\t <br\> -- Menander'}
 // Cool, now just trust me for this next one...
-  // Set the inner HMTL of the paragraph element to the value of the fortune key in the fortune object
-  
+// Set the inner HMTL of the paragraph element to the value of the fortune key in the fortune object
+txt.innerHTML += fortune['fortune'];
 // // // // // // // // // // // //  // // // //
 // PAUSE: PLEASE WAIT FOR FURTHER INSTRUCTION //
 // // // // // // // // // // // //  // // // //
