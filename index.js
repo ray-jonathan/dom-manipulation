e0f5b5293208ddeb3900190e8833ed2d776e923b
Kyle <360klacy@gmail.com>
2019-07-19 10:44:36 -0400

    create another element

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
 // Grab the body element of the page
-  const body = document.body;
+const body = document.body;
 
 // Create a header element and append it to the body
 
+const header = document.createElement('header')
+body.appendChild(header)
+
 // Style the header with a height, custom background color, 
 
+header.style.height = "200px";
+header.style.backgroundColor = "blue";
+
 // Create and add a h1 element to the header, add some text, and center it
+const title = document.createElement('h1')
+header.appendChild(title)
+
+title.innerHTML = "hello beautiful";
+header.style.display = "flex";
+
 
 // No, like center it in the header. Middle of the middle!
 
+header.style.justifyContent = "center"
+header.style.alignItems = "center";
+
 // Customize the header title you made in some way, such as font family, style, or size. Color too! Get creative
+
+title.style.fontFamily = "serif";
+title.style.fontStyle = "italic"
+title.style.fontSize = "3rem";
+title.style.color = "orange";
 
 // // // // // // // // // // // //  // // // // //
 // SWITCH: PLEASE HAND OFF CONTROL TO A PARTNER  //
 // // // // // // // // // // // //  // // // // // 
 
 // Make another element, this time a div that'll house the rest of our content
-
+const houseContent = document.createElement('div')
 // Append it to the body
-
+body.appendChild(houseContent);
 // Create an image element
+const img = document.createElement('img')
+houseContent.appendChild(img);
 
 // Set the source attribute of the image element to `https://i.imgur.com/5bMVQtj.png` and append it to the content div
-
+img.src = `https://i.imgur.com/5bMVQtj.png`;
 // Center this image on the page using your favorite method. (I'm using flexbox, but you do you)
   // Note: don't forget to give your content div a height like I did 😳
+houseContent.style.display="flex";
+houseContent.style.justifyContent= 'center';
+houseContent.style.height='500px';
+img.style.height="50%";
+houseContent.style.alignItems="center";
+
 
 // Now's a good time to style the body background too, I guess
+houseContent.style.backgroundColor= 'lightblue';
 
 // // // // // // // // // // // //  // // // // //
 // SWITCH: PLEASE HAND OFF CONTROL TO A PARTNER  //
